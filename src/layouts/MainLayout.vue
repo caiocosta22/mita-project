8ff4f555f7cdeb84f8e27525dae3118ce36afcd7
caiocosta22 <caiocosta22@gmail.com>
2023-08-22 17:45:59 -0300

    Primeiro commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,12 @@
+<template lang="pug">
+q-layout(view="lHh Lpr lFf")
+  q-header(elevated)
+    Topbar
+  router-view
+  Footer
+</template>
+
+<script setup>
+import Topbar from "../components/LayoutComponents/Topbaritems.vue";
+import Footer from "../components/LayoutComponents/Footeritems.vue";
+</script>
---
+++ src/pages/HomePage.vue
@@ -0,0 +1,13 @@
+<template lang="pug">
+q-page-container
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+  q-page-container{
+    display: flex;
+    flex-direction: column;
+  }
+</style>
