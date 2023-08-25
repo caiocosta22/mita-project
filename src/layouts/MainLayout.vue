7553c29c95b5b335952858732ae4fbd11cc214ae
Caio Costa <caiocosta22@gmail.com>
2023-08-24 23:30:05 -0300

    Tentativa menu transparente

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
 q-layout(view="lHh Lpr lFf")
-  q-header(elevated)
+  q-header(:class="headerClass")
     Topbar
     Cabecalho
     Navbar
@@ -14,4 +14,31 @@ import Topbar from "../components/LayoutComponents/Topbaritems.vue";
 import Cabecalho from "../components/LayoutComponents/Cabecalhoitems.vue";
 import Navbar from "../components/LayoutComponents/Navitems.vue";
 import Footer from "../components/LayoutComponents/Footeritems.vue";
+import { ref, onMounted, onBeforeUnmount } from "vue";
+
+const headerClass = ref("transparent-bg");
+
+const handleScroll = () => {
+  headerClass.value = window.scrollY > 0 ? "white-bg" : "transparent-bg";
+};
+
+onMounted(() => {
+  window.addEventListener("scroll", handleScroll);
+});
+
+onBeforeUnmount(() => {
+  window.removeEventListener("scroll", handleScroll);
+});
 </script>
+
+<style scoped>
+.transparent-bg {
+  background-color: transparent;
+  /* Outros estilos para o fundo transparente */
+}
+
+.white-bg {
+  background-color: white;
+  /* Outros estilos para o fundo branco */
+}
+</style>
