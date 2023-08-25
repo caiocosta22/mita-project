c2d5e53f980914378d7a5b2bb11f71e2e558e734
caiocosta22 <caiocosta22@gmail.com>
2023-08-25 15:27:16 -0300

    Menu transparente 25082023

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,17 +1,8 @@
 <template lang="pug">
 q-layout(view="lHh Lpr lFf")
-  q-header(elevated)
-    Topbar
-    Cabecalho
-    Navbar
   router-view
-  Footer
-
 </template>
 
-<script setup>
-import Topbar from "../components/LayoutComponents/Topbaritems.vue";
-import Cabecalho from "../components/LayoutComponents/Cabecalhoitems.vue";
-import Navbar from "../components/LayoutComponents/Navitems.vue";
-import Footer from "../components/LayoutComponents/Footeritems.vue";
-</script>
+<style scoped>
+
+</style>
