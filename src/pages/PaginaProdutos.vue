cf8e4c511c27cb410a50a037de3afa381fcdffd4
caiocosta22 <caiocosta22@gmail.com>
2023-09-04 11:26:49 -0300

    União de componentes para custom de produto

--- src/pages/PaginaProdutos.vue
+++ src/pages/PaginaProdutos.vue
@@ -1,17 +1,16 @@
 <script setup>
 // import {ref} from "vue";
-import Miniaturas from "../components/PaginaProdutos/Miniaturas.vue";
-import FotoPrincipal from "../components/PaginaProdutos/FotoPrincipal.vue";
-import Descricao from "../components/PaginaProdutos/Descricao.vue";
+// import Miniaturas from "../components/PaginaProdutos/Miniaturas.vue";
+// import FotoPrincipal from "../components/PaginaProdutos/FotoPrincipal.vue";
+// import Descricao from "../components/PaginaProdutos/Descricao.vue";
+import DetalhesProduto from "../components/PaginaProdutos/DetalhesProduto.vue";
 import Marketing from "src/components/Layout/Marketing.vue";
 </script>
 
 <template lang="pug">
 q-page-container
   .row.justify-center.q-gutter-sm
-    Miniaturas
-    FotoPrincipal
-    Descricao
+    DetalhesProduto
   Marketing
 </template>
 
