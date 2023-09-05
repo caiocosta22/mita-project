015d6f3a56b7a9ff68263d0250de0519ccdf5141
caiocosta22 <caiocosta22@gmail.com>
2023-09-05 17:38:56 -0300

    Inicio tela de categorias

--- src/components/PaginaCategorias/Filtro.vue
+++ src/components/PaginaCategorias/Filtro.vue
@@ -5,21 +5,23 @@ const modelos = ref(["DINHEIRO", "PIX", "CARTAO DE CREDITO"]);
 </script>
 <template lang="pug">
 .justify-evenly.q-gutter-sm.flex.row
-  .column
-    div.marcas.column(style="width: 257px;height: 228px;")
+  .column.q-pa-md
+    div.marcas.column(style="width: 257px;height: 257px;")
       q-select(bordeless v-model="model" :options="marcas" label-color="black" label="Marcas" color="black")
-      q-checkbox(v-model="right" label="Label on Right")
-      q-checkbox(v-model="right" label="Label on Right")
-      q-checkbox(v-model="right" label="Label on Right")
-      q-checkbox(v-model="right" label="Label on Right")
-      q-checkbox(v-model="right" label="Label on Right")
-    div.modelos.column(style="width: 257px;height: 228px;")
+      q-separator(color="black")
+      q-checkbox(v-model="Left" label="iPhone" text-color="black" color="black")
+      q-checkbox(v-model="Left" label="Samsung" text-color="black" color="black")
+      q-checkbox(v-model="Left" label="LG" text-color="black" color="black")
+      q-checkbox(v-model="Left" label="Motorola" text-color="black" color="black")
+      q-checkbox(v-model="Left" label="Xiaomi" text-color="black" color="black")
+    div.modelos.column(style="width: 257px;height: 257px;")
       q-select(bordeless v-model="model" :options="modelos" label-color="black" label="Modelo" color="black")
-      q-checkbox(v-model="right" label="Label on Right")
-      q-checkbox(v-model="right" label="Label on Right")
-      q-checkbox(v-model="right" label="Label on Right")
-      q-checkbox(v-model="right" label="Label on Right")
-      q-checkbox(v-model="right" label="Label on Right")
+      q-separator(color="black")
+      q-checkbox(v-model="Left" label="iPhone X" text-color="black" color="black")
+      q-checkbox(v-model="Left" label="iPhone XR" text-color="black" color="black")
+      q-checkbox(v-model="Left" label="iPhone 11" text-color="black" color="black")
+      q-checkbox(v-model="Left" label="iPhone 11 PRO" text-color="black" color="black")
+      q-checkbox(v-model="Left" label="iPhone 12" text-color="black" color="black")
 </template>
 
 <style scoped>
