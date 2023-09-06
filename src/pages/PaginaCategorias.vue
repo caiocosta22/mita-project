bf90a288771c75836d6e0ad60f12cef876c48333
Caio Costa <caiocosta22@gmail.com>
2023-09-05 22:56:21 -0300

    Pagina de categorias finalizada

--- src/pages/PaginaCategorias.vue
+++ src/pages/PaginaCategorias.vue
@@ -1,10 +1,12 @@
 <script setup>
 import Filtro from "../components/PaginaCategorias/Filtro.vue";
+import Produtos from "../components/PaginaCategorias/Produtos.vue";
 </script>
 
 <template lang="pug">
-q-page-container.row
+q-page-container.row.q-pa-md.q-gutter-md.q-ml-md(style="flex-wrap:nowrap")
   Filtro
+  Produtos
 </template>
 
 <style scoped>
