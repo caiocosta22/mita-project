eef9d252a7b5164d8ea42025bd31e53fcfc5f490
Caio Costa <caiocosta22@gmail.com>
2023-09-22 15:38:53 -0300

    Ajustes refs checkbox

--- src/components/PaginaCategorias/Filtro.vue
+++ src/components/PaginaCategorias/Filtro.vue
@@ -1,7 +1,15 @@
 <script setup>
 import { ref } from "vue";
 const model = ref([]);
-const val = ref([]);
+const val1 = ref(false);
+const val2 = ref(false);
+const val3 = ref(false);
+const val4 = ref(false);
+const val5 = ref(false);
+const val6 = ref(false);
+const val7 = ref(false);
+const val8 = ref(false);
+const val9 = ref(false);
 const isChecked = ref(false);
 
 const marcas = ref(["DINHEIRO", "PIX", "CARTAO DE CREDITO"]);
@@ -18,18 +26,18 @@ div.justify-evenly.q-gutter-sm.flex.row
       q-select(bordeless v-model="model" :options="marcas" label-color="black" label="Categorias" color="black" blocked)
       q-separator(color="black")
       q-checkbox(v-model="isChecked" label="iPhone" color="black")
-      q-checkbox(v-model="val" label="Samsung" text-color="black" color="black")
-      q-checkbox(v-model="val" label="LG" text-color="black" color="black")
-      q-checkbox(v-model="val" label="Motorola" text-color="black" color="black")
-      q-checkbox(v-model="val" label="Xiaomi" text-color="black" color="black")
+      q-checkbox(v-model="val1" label="Samsung" text-color="black" color="black")
+      q-checkbox(v-model="val2" label="LG" text-color="black" color="black")
+      q-checkbox(v-model="val3" label="Motorola" text-color="black" color="black")
+      q-checkbox(v-model="val4" label="Xiaomi" text-color="black" color="black")
     div.modelos.column(style="width: 257px;height: 257px;")
       q-select(bordeless v-model="model" :options="modelos" label-color="black" label="Modelo" color="black")
       q-separator(color="black")
-      q-checkbox(v-model="val" label="iPhone X" text-color="black" color="black" )
-      q-checkbox(v-model="val" label="iPhone XR" text-color="black" color="black")
-      q-checkbox(v-model="val" label="iPhone 11" text-color="black" color="black")
-      q-checkbox(v-model="val" label="iPhone 11 PRO" text-color="black" color="black")
-      q-checkbox(v-model="val" label="iPhone 12" text-color="black" color="black")
+      q-checkbox(v-model="val5" label="iPhone X" text-color="black" color="black" )
+      q-checkbox(v-model="val6" label="iPhone XR" text-color="black" color="black")
+      q-checkbox(v-model="val7" label="iPhone 11" text-color="black" color="black")
+      q-checkbox(v-model="val8" label="iPhone 11 PRO" text-color="black" color="black")
+      q-checkbox(v-model="val9" label="iPhone 12" text-color="black" color="black")
 </template>
 
 <style scoped>
