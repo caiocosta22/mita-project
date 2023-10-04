233d4a2a39d942765329c3faa86d116eef4ca535
Caio Costa <caiocosta22@gmail.com>
2023-10-04 16:30:19 -0300

    topbar finalizada

--- src/components/Layout/Topbar.vue
+++ src/components/Layout/Topbar.vue
@@ -1,31 +1,94 @@
 <script setup>
-import { ref } from "vue";
-const fretes = [
-  "Prazo de Retirada / Envio 7 dias úteis",
-  "Enviamos para todo país!"
-];
-const padrao = ref(fretes[0]);
+import { ref, onBeforeMount } from "vue";
+import axios from "src/boot/axios";
+
+const slide = ref(1);
+const autoplay = ref(true);
+const itsLoading = ref(true);
+
+const bannersAnuncio = ref([
+  {
+    fotoWebp: "https://cs210033fff90d2f7ac.blob.core.windows.net/mitaoficial/banner/2023-10-04T15:44:04.670_110.webp",
+    id: 0,
+    image: "",
+    ordem: 0,
+    posicionamento: "anuncio"
+  },
+  {
+    fotoWebp: "https://cs210033fff90d2f7ac.blob.core.windows.net/mitaoficial/banner/2023-10-04T15:44:04.670_110.webp",
+    id: 0,
+    image: "",
+    ordem: 0,
+    posicionamento: "anuncio"
+  },
+  {
+    fotoWebp: "https://cs210033fff90d2f7ac.blob.core.windows.net/mitaoficial/banner/2023-10-04T15:44:04.670_110.webp",
+    id: 0,
+    image: "",
+    ordem: 0,
+    posicionamento: "anuncio"
+  }
+]);
+
+const interval = 5 * 1000;
+
+const padrao = ref(bannersAnuncio.value[0]);
+
 let contador = 0;
+
 function transicao () {
-  if (contador === fretes.length - 1) {
-    padrao.value = fretes[contador];
+  if (contador === bannersAnuncio.value.length - 1) {
+    padrao.value = bannersAnuncio[contador];
     contador = 0;
   } else {
-    padrao.value = fretes[contador];
+    padrao.value = bannersAnuncio[contador];
     contador++;
   }
 }
-const interval = 5 * 1000;
+
 setInterval(() => {
   transicao();
 }, interval);
+
+async function searchTopBanners () {
+  try {
+    const banners = await axios.get("https://mitaoficial.elevarcommerceapi.com.br/HandoverMetasWS/webapi/handover/portal/bannerService/allEcommerce").then(e => e.data);
+    if (banners.length) bannersAnuncio.value = banners.filter(banner => banner.posicionamento === "anuncio");
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+onBeforeMount(async () => {
+  itsLoading.value = true;
+  await searchTopBanners();
+  itsLoading.value = false;
+});
 </script>
 
 <template lang = "pug">
-q-toolbar.justify-center.topbar(
-  style="background-color: black; min-height: 35px; height:40px" )
-  span.text-center(
-    style="color:white") {{ padrao }}
+div(
+  v-show="!itsLoading"
+)
+  q-carousel.cursor-pointer(
+    animated
+    v-model="slide"
+    infinite
+    swipeable
+    autoplay="true"
+    transition-prev="slide-right"
+    transition-next="slide-left"
+    style="max-height: 32px;"
+  )
+    template(
+      v-for="(banner, index) in bannersAnuncio"
+      :key="index"
+    )
+      q-carousel-slide.slide(
+        :name="index"
+        :img-src="banner.fotoWebp"
+        style="height: 32px;"
+        )
 </template>
 
 <style scoped>
