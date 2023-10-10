5cd83c94abe1a539771a92e90e50546954f05bfe
icaro.freitas <icaro.freitas@icolabora.com>
2023-10-10 13:42:18 -0300

    Arrumando localstorage getCartItem

--- src/helpers/getCartItems.js
+++ src/helpers/getCartItems.js
@@ -1,19 +1,18 @@
 import axios from "axios";
-import { useQuasar } from "quasar";
-const $q = useQuasar();
+import { LocalStorage } from "quasar";
 
 async function getCartItems () {
-  const cartId = $q.localStorage.getItem("cartIdBackend") || undefined;
+  const cartId = LocalStorage.getItem("cartIdBackend") || undefined;
   let cartItems = [];
-  let quantidadeCarrinho = $q.localStorage.getItem("quantidadeCarrinho") || 0;
-  $q.localStorage.set("quantidadeCarrinho", quantidadeCarrinho);
+  let quantidadeCarrinho = LocalStorage.getItem("quantidadeCarrinho") || 0;
+  LocalStorage.set("quantidadeCarrinho", quantidadeCarrinho);
   try {
     if (cartId) {
       const cart = await axios.post(`https://mitaoficial.elevarcommerceapi.com.br/HandoverMetasWS/webapi/handover/portal/cartService/getCart/${cartId}/-1`);
       cartItems = cart.data || cart.response.data;
       if (cartItems !== "Nenhum carrinho válido encontrado") {
         quantidadeCarrinho = cartItems.items?.length;
-        $q.localStorage.set("quantidadeCarrinho", quantidadeCarrinho);
+        LocalStorage.set("quantidadeCarrinho", quantidadeCarrinho);
       }
     }
   } catch (e) {
