34987124237176303fcf3995cef4967097d1f00d
caiocosta22 <caiocosta22@gmail.com>
2023-11-07 18:06:21 -0300

    cookie finalizado

--- src/layouts/LayoutLimpo.vue
+++ src/layouts/LayoutLimpo.vue
@@ -3,38 +3,52 @@ q-layout(view="lHh Lpr lFf")
   router-view
   CadastroEmail
   Rodape
-  </template>
+</template>
 
 <script setup>
 import Rodape from "../components/Layout/Rodape.vue";
-import CadastroEmail from "../components//Layout/CadastroEmail.vue";
+import CadastroEmail from "../components/Layout/CadastroEmail.vue";
 import { onMounted } from "vue";
-import { useQuasar } from "quasar";
+import { useQuasar, Cookies } from "quasar";
+
 const $q = useQuasar();
-function positioned () {
-  $q.dialog({
-    message: "Políticas de Privacidade: Ao clicar em Ok, você concorda com o armazenamento de cookies no seu dispositivo para melhorar a navegação no site, analisar a utilização do site e ajudar nas nossas iniciativas de marketing.",
-    ok: {
-      push: true,
-      color: "black",
-      padding: "8px 60px"
-    },
-    persistent: true,
-    position: "bottom",
-    fullWidth: true,
-    seamless: true,
-    style: {
-      background: "rgba(255,255,255,0.8)",
-      fontWeight: "600",
-      fontSize: "16px",
-      textAlign: "Left",
-      marginBlock: "Center"
-    },
-    square: true
-  });
+const options = {
+  expires: "365d"
+};
+const politicas = $q.cookies.has("politicas");
+
+function setcookies () {
+  $q.cookies.set("politicas", true, options);
 }
+
+function showCookieDialog () {
+  if (!politicas) {
+    $q.dialog({
+      message: "Políticas de Privacidade: Ao clicar em Ok, você concorda com o armazenamento de cookies no seu dispositivo para melhorar a navegação no site, analisar a utilização do site e ajudar nas nossas iniciativas de marketing.",
+      ok: {
+        push: true,
+        color: "black",
+        padding: "8px 60px",
+        onclick: setcookies
+      },
+      persistent: true,
+      position: "bottom",
+      fullWidth: true,
+      seamless: true,
+      style: {
+        background: "rgba(255,255,255,0.8)",
+        fontWeight: "600",
+        fontSize: "16px",
+        textAlign: "left",
+        marginBlock: "center"
+      },
+      square: true
+    });
+  }
+}
+
 onMounted(() => {
-  positioned();
+  showCookieDialog();
 });
 </script>
 
