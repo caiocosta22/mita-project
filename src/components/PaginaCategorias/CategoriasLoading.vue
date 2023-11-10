6e6e1a296f50140b89260737b6c8776a2bb3e895
Caio Costa <caiocosta22@gmail.com>
2023-11-10 17:38:54 -0300

    ajuste loading

--- src/components/PaginaCategorias/CategoriasLoading.vue
+++ src/components/PaginaCategorias/CategoriasLoading.vue
@@ -3,34 +3,11 @@
 
 <template lang="pug">
 div.row.justify-center.col.q-pa-md
-  div.col-12.row.q-gutter-xl.q-pl-xl.q-mt-xl
-    q-skeleton.col-3(
-      height="500px"
+  div.col-12.row.q-pt-lg
+    q-skeleton.col-12(
+      height="1200px"
     )
-    q-skeleton.col-3(
-      height="500px"
-    )
-    q-skeleton.col-3(
-      height="500px"
-    )
-    q-skeleton.col-3(
-      height="500px"
-    )
-    q-skeleton.col-3(
-      height="500px"
-    )
-    q-skeleton.col-3(
-      height="500px"
-    )
-    q-skeleton.col-3(
-      height="500px"
-    )
-    q-skeleton.col-3(
-      height="500px"
-    )
-    q-skeleton.col-3(
-      height="500px"
-    )
+
 </template>
 
 <style scoped>
