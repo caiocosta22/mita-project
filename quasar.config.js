91761059fce60aa93bd1fe0b9bd9d875211b975d
caiocosta22 <caiocosta22@gmail.com>
2023-11-21 16:14:04 -0300

    retornando para hash

--- quasar.config.js
+++ quasar.config.js
@@ -64,7 +64,7 @@ module.exports = configure(function (ctx) {
         node: "node16"
       },
 
-      vueRouterMode: "history", // available values: 'hash', 'history'
+      vueRouterMode: "hash", // available values: 'hash', 'history'
       uglifyOptions: {
         compress: { drop_console: true }
       },
