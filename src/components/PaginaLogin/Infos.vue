4526bf90c81b8388055b3803fd900f0fc7adbce0
Caio Costa <caiocosta22@gmail.com>
2024-02-15 12:32:21 -0300

    placeholders ativados

--- src/components/PaginaLogin/Infos.vue
+++ src/components/PaginaLogin/Infos.vue
@@ -1,6 +1,48 @@
 <script setup>
-import { ref } from "vue";
+import { ref, onBeforeMount } from "vue";
+import { useQuasar } from "quasar";
+import axios from "axios";
 
+const $q = useQuasar();
+const id = $q.localStorage.getItem("idclient");
+
+const datacliente = ref(
+  {
+    celular: "85997244223",
+    dataNascimento: "10012002",
+    dsEmail: "caioteste@teste.com",
+    endereco: {
+      bairro: "",
+      cep: "",
+      cidade: "",
+      complemento: "",
+      georeferencia: {
+        endereco: "",
+        id: 0,
+        latitude: 0,
+        longitude: 0
+      },
+      id: 0,
+      logradouro: "",
+      numero: 0,
+      uf: ""
+    },
+    foto: "",
+    idCliente: 1,
+    ieIsento: false,
+    limiteCredito: 0,
+    login: "",
+    nmCliente: "Caio",
+    nmRazao: "",
+    nrCpfCnpj: "123.456.789-32",
+    nrIe: "",
+    nrTelefone: "",
+    saldoAberto: 0,
+    saldoVencido: 0,
+    sobrenome: "Costa",
+    tipo: ""
+  }
+);
 const nome = ref("");
 const cpf = ref("");
 const sobrenome = ref("");
@@ -8,70 +50,93 @@ const nascimento = ref("");
 const telefone = ref("");
 const email = ref("");
 
+async function searchInfos () {
+  try {
+    const data = await axios.get(`https://mitaoficial.elevarcommerceapi.com.br/HandoverMetasWS/webapi/handover/portal/clienteService/getClienteEcommerce/${id}`).then(e => e.data);
+    if (data.length) {
+      datacliente.value = data;
+      console.log("INFORMACOES DO CLIENTE:", data);
+    }
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+onBeforeMount(async () => {
+  await searchInfos();
+});
 </script>
 
 <template lang="pug">
 div.container
-  div.interno
-    span.subtitulo(
-      style="font-size: 24px; margin-left: 15px;"
-    ) Dados Pessoais
-  div.interno
+  template(
+    v-if="datacliente"
+  )
+    div.interno
+      span.subtitulo(
+        style="font-size: 24px; margin-left: 15px;"
+      ) Dados Pessoais
+    div.interno
+      div.input
+        .subtitulo Nome
+        q-input(
+          outlined
+          color="black"
+          v-model="nome"
+          :placeholder="datacliente.nmCliente"
+        )
+      div.input
+        .subtitulo Sobrenome
+        q-input(
+          outlined
+          color="black"
+          v-model="sobrenome"
+          :placeholder="datacliente.sobrenome"
+        )
+      div.input
+        .subtitulo CPF
+        q-input(
+          outlined
+          color="black"
+          v-model="cpf"
+          :placeholder="datacliente.nrCpfCnpj"
+          disable
+          style="background-color: rgba(100,100,100,0.1);"
+        )
+      div.input
+        .subtitulo Data Nascimento*
+        q-input(
+          outlined
+          color="black"
+          v-model="nascimento"
+          :placeholder="datacliente.dataNascimento"
+        )
+      div.input
+        .subtitulo Telefone*
+        q-input(
+          outlined
+          color="black"
+          v-model="telefone"
+          :placeholder="datacliente.celular"
+        )
+      div.input
+        .subtitulo E-Mail
+        q-input(
+          outlined
+          color="black"
+          v-model="email"
+          :placeholder="datacliente.dsEmail"
+          disable
+          style="background-color: rgba(100,100,100,0.1);"
+        )
     div.input
-      .subtitulo Nome
-      q-input(
-        outlined
+      q-btn(
         color="black"
-        v-model="nome"
-        placeholder="Ex: João"
+        push
       )
-    div.input
-      .subtitulo Sobrenome
-      q-input(
-        outlined
-        color="black"
-        v-model="sobrenome"
-        placeholder="Ex: Da silva"
-      )
-    div.input
-      .subtitulo CPF
-      q-input(
-        outlined
-        color="black"
-        v-model="cpf"
-        placeholder="Ex: 60700-700"
-      )
-    div.input
-      .subtitulo Data Nascimento*
-      q-input(
-        outlined
-        color="black"
-        v-model="nascimento"
-        placeholder="Ex: 01/01/1990"
-      )
-    div.input
-      .subtitulo Telefone*
-      q-input(
-        outlined
-        color="black"
-        v-model="telefone"
-        placeholder="Ex: (85)91111-1111"
-      )
-    div.input
-      .subtitulo E-Mail
-      q-input(
-        outlined
-        color="black"
-        v-model="email"
-      )
-  div.input
-    q-btn(
-      color="black"
-      push
-    )
-      span(
-        style="color: #fff;"
-      ) SALVAR
+        span(
+          style="color: #fff;"
+        ) SALVAR
 </template>
 
 <style scoped>
